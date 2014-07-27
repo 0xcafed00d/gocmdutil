6bc409eb93969d58b627674eec99c5cc6e77b3c7
SimulatedSimian <neomech@gmail.com>
2014-07-27 01:38:38 +0100

    testing walk functions

--- gtx/gtx.go
+++ gtx/gtx.go
@@ -1,1 +1,77 @@
- 
+package main
+
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	//	"github.com/simulatedsimian/neo"
+	"os"
+	"path/filepath"
+	"unicode/utf8"
+)
+
+func printAt(x, y int, s string, fg, bg termbox.Attribute) {
+	for len(s) > 0 {
+		r, rlen := utf8.DecodeRuneInString(s)
+		termbox.SetCell(x, y, r, fg, bg)
+		s = s[rlen:]
+		x++
+	}
+}
+
+func printAtDef(x, y int, s string) {
+	printAt(x, y, s, termbox.ColorDefault, termbox.ColorDefault)
+}
+
+func WalkFunc(path string, info os.FileInfo, err error) error {
+	if info.IsDir() && path != "." {
+		fmt.Println("+", path)
+		return filepath.SkipDir
+	}
+	fmt.Println(path)
+	return nil
+}
+
+func test() {
+	filepath.Walk(".", WalkFunc)
+}
+
+func termtest() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	termbox.HideCursor()
+
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			printAt(0, 1, fmt.Sprint(ev), termbox.ColorDefault, termbox.ColorDefault)
+			termbox.Flush()
+
+			switch ev.Key {
+			case termbox.KeyEsc:
+				return
+			case termbox.KeyArrowUp:
+			case termbox.KeyArrowDown:
+			}
+
+			termbox.Flush()
+
+		case termbox.EventResize:
+			x, y := ev.Width, ev.Height
+			printAt(0, 0, fmt.Sprintf("[%d, %d] ", x, y), termbox.ColorDefault, termbox.ColorDefault)
+			termbox.Flush()
+		}
+	}
+
+	termbox.Flush()
+
+}
+
+func main() {
+
+	test()
+
+}
