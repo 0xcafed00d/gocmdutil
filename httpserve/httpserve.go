5095ee725a13b7a5da7be6186c0e89219ff2c83f
Lee Witek <neomech@gmail.com>
2015-03-25 01:04:26 +0000

    added option to set port number

--- httpserve/httpserve.go
+++ httpserve/httpserve.go
@@ -3,9 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+
+	port := "8080"
+
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir("."))))
 }
