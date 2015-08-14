c71cffc86697ecd186361c8c8c91467fdc75c523
Lee Witek <neomech@gmail.com>
2015-08-14 13:38:07 +0100

    added setting exit code

--- dummyproc/rerun.go
+++ dummyproc/rerun.go
@@ -20,6 +20,21 @@ func main() {
 		if err == nil {
 			fmt.Fprintf(os.Stderr, "Sleeping for %d Seconds\n", sleeptime)
 			time.Sleep(time.Duration(sleeptime) * time.Second)
+		} else {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
 		}
 	}
+
+	if len(os.Args) >= 3 {
+		exitcode, err := strconv.Atoi(os.Args[2])
+		if err == nil {
+			fmt.Fprintf(os.Stderr, "Exiting with code: %d\n", exitcode)
+			os.Exit(exitcode)
+		} else {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+	}
+
 }
