18ef7005b8369ab84f8e9c61cc4b1bd56397db13
Lee Witek <neomech@gmail.com>
2015-09-05 23:16:01 +0100

    print signals

--- dummyproc/dummyproc.go
+++ dummyproc/dummyproc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"strconv"
 	"time"
 )
@@ -15,11 +16,15 @@ func main() {
 		fmt.Printf("arg: %d: %s\n", i, a)
 	}
 
+	var err error
+	var exitCode int
+	var sleepyTime time.Duration
+
 	if len(os.Args) >= 2 {
 		sleeptime, err := strconv.Atoi(os.Args[1])
 		if err == nil {
 			fmt.Fprintf(os.Stderr, "Sleeping for %d Seconds\n", sleeptime)
-			time.Sleep(time.Duration(sleeptime) * time.Second)
+			sleepyTime = time.Duration(sleeptime) * time.Second
 		} else {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(-1)
@@ -27,14 +32,23 @@ func main() {
 	}
 
 	if len(os.Args) >= 3 {
-		exitcode, err := strconv.Atoi(os.Args[2])
-		if err == nil {
-			fmt.Fprintf(os.Stderr, "Exiting with code: %d\n", exitcode)
-			os.Exit(exitcode)
-		} else {
+		exitCode, err = strconv.Atoi(os.Args[2])
+		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(-1)
 		}
 	}
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	select {
+	case s := <-c:
+		fmt.Fprintln(os.Stderr, "Signal Received: ", s)
+
+	case <-time.After(sleepyTime):
+		fmt.Println("timed out")
+	}
+
+	fmt.Printf("Exiting with code: %d\n", exitCode)
+	os.Exit(exitCode)
 }
